1279b88d0775b15f4a614cde76576dc93626e90f
bobrekjiri <bobrekjiri@gmail.com>
2015-04-18 18:00:42 +0200

    Updated Common line equation test

--- src/Test/CommonLineEquationTest.java
+++ src/Test/CommonLineEquationTest.java
@@ -1,12 +1,11 @@
-package Test;
+package test;
 
 import static org.junit.Assert.assertEquals;
+import math.CommonLineEquation;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.newdawn.slick.geom.Vector2f;
-
-import Math.CommonLineEquation;
 
 public class CommonLineEquationTest {
 
@@ -18,18 +17,27 @@ public class CommonLineEquationTest {
     public void setUp() {
         equation1 = new CommonLineEquation(-1, 3, -5);
         equation2 = new CommonLineEquation(2, 3, -17);
-        equation3 = new CommonLineEquation(0, 1, -10);
+
+        equation3 = new CommonLineEquation(new Vector2f(1, 2), new Vector2f(-1, 3));
     }
 
     @Test
-    public void test() {
+    public void testCommonPoint() {
         Vector2f commonPoint = equation1.commonPoint(equation2);
 
-        assertEquals("Expected X value is 4", 4, commonPoint.x, 0.001);
-        assertEquals("Expected Y value is 3", 3, commonPoint.y, 0.001);
+        assertEquals("Unexpected common point X value", 4, commonPoint.x, 0.001);
+        assertEquals("Unexpected common point Y value", 3, commonPoint.y, 0.001);
 
-        Vector2f commonPoint2 = equation1.commonPoint(equation3);
-        System.out.println(commonPoint2.toString());
+        assertEquals("Unexpected common point X value", 4, commonPoint.x, 0.001);
+        assertEquals("Unexpected common point Y value", 3, commonPoint.y, 0.001);
+    }
+
+    @Test
+    public void testPointAndVectorConstructor() {
+
+        assertEquals("Unexpected A coeficient value", -1, equation3.a, 0.001);
+        assertEquals("Unexpected B coeficient value", 3, equation3.b, 0.001);
+        assertEquals("Unexpected C coeficient value", -5, equation3.c, 0.001);
     }
 
 }
